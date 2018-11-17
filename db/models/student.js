fbcf2831cd9686c85a9b469f68e1e4f5d4f1d90d
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2018-11-17 11:59:48 -0500

    cycle 1 complete

--- db/models/student.js
+++ db/models/student.js
@@ -22,9 +22,8 @@ const Student = db.define('student', {
 });
 
 Student.beforeCreate(student => {
-  student.firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
-  student.lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
-  return student;
+  student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1);
+  student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1);
 });
 
 module.exports = Student;
--- routes/students.js
+++ routes/students.js
@@ -1,10 +1,5 @@
 const router = require('express').Router();
-const Student = require('../db/models/student');
-// const Test = require('../db/models/test');
 
-router.get('/', async (req, res, next) => {
-  agent = await Student.findAll()
-  return agent
-})
+
 
 module.exports = router;
